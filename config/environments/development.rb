62b02a85e64ba38accff0aad9e13bead69e60c9d
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2022-03-06 08:01:11 +0000

    Finish

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.hosts << "9c8108d39e8d4f56b26f0aa53932234f.vfs.cloud9.us-east-1.amazonaws.com"
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
